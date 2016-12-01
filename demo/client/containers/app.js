294a99911c98488356d654ac1c4797f7155773b0
Phil <phil3903@gmail.com>
2016-12-01 12:17:21 -0500

    Updated demo

--- demo/client/containers/app.js
+++ demo/client/containers/app.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { testCall, testCall1 } from '../actions/test_actions'
+import _ from 'lodash'
+import { sankeyData } from '../../../src/components/charts/data'
 
-import { DateTimePicker, Button, Table, Dropdown, DropdownItem, ElementLoader } from '../../../src/index'
+import { DateTimePicker, Button, Table, Dropdown, DropdownItem, ElementLoader, TrajectoryChart, LineChart, BarChart, Sankey } from '../../../src/index'
+
 
 class App extends React.Component {
 
@@ -47,6 +50,145 @@ class App extends React.Component {
 
     const { tableData } = this.state
 
+    const trajectoryData = [
+      {
+        "sale": 202,
+        "year": 2000
+      },
+
+      {
+        "sale": 179,
+        "year": 2002
+      },
+
+      {
+        "sale": 154,
+        "year": 2003
+      },
+
+      {
+        "sale": 215,
+        "year": 2001
+      },
+
+      {
+        "sale": 260,
+        "year": 2010
+      }
+    ];
+
+    const lineData = [
+      {
+        name: 'Data Set 1',
+        values: [
+          {
+            "date": "1-May-12",
+            "close": 58.13
+          },
+          {
+            "date": "30-Apr-12",
+            "close": 53.98
+          },
+          {
+            "date": "27-Apr-12",
+            "close": 67
+          },
+          {
+            "date": "26-Apr-12",
+            "close": 89.7
+          },
+          {
+            "date": "25-Apr-12",
+            "close": 99
+          },
+          {
+            "date": "24-Apr-12",
+            "close": 130.28
+          },
+          {
+            "date": "23-Apr-12",
+            "close": 166.7
+          },
+          {
+            "date": "20-Apr-12",
+            "close": 234.98
+          },
+          {
+            "date": "19-Apr-12",
+            "close": 345.44
+          },
+          {
+            "date": "18-Apr-12",
+            "close": 443.34
+          },
+          {
+            "date": "17-Apr-12",
+            "close": 543.7
+          },
+          {
+            "date": "16-Apr-12",
+            "close": 580.13
+          },
+          {
+            "date": "13-Apr-12",
+            "close": 605.23
+          },
+          {
+            "date": "12-Apr-12",
+            "close": 622.77
+          },
+          {
+            "date": "11-Apr-12",
+            "close": 626.2
+          },
+          {
+            "date": "10-Apr-12",
+            "close": 628.44
+          },
+          {
+            "date": "9-Apr-12",
+            "close": 636.23
+          },
+          {
+            "date": "5-Apr-12",
+            "close": 633.68
+          },
+          {
+            "date": "4-Apr-12",
+            "close": 624.31
+          },
+          {
+            "date": "3-Apr-12",
+            "close": 629.32
+          },
+          {
+            "date": "2-Apr-12",
+            "close": 618.63
+          },
+          {
+            "date": "30-Mar-12",
+            "close": 599.55
+          },
+          {
+            "date": "29-Mar-12",
+            "close": 609.86
+          },
+          {
+            "date": "28-Mar-12",
+            "close": 617.62
+          },
+          {
+            "date": "27-Mar-12",
+            "close": 614.48
+          },
+          {
+            "date": "26-Mar-12",
+            "close": 606.98
+          }
+        ]
+      }
+    ]
+
     const style = {
       container1: { border: '1px solid black', margin: 5, backgroundColor: '#d4d4d4'},
       container2: { width: 300, height: 200, border: '1px solid black', margin: 5}
@@ -54,7 +196,7 @@ class App extends React.Component {
 
 
     return(
-      <div style={{ width: 500}}>
+      <div style={{ height: '100vh'}}>
         <Button
           text={ 'Filter Events' }
           textColor={ 'white' }
@@ -68,9 +210,6 @@ class App extends React.Component {
           isTimePicker
           onChange={ this.handleDateChange }
           timeFormat={ 'hh:mm a' }/>
-        <div>
-
-
 
 
 
@@ -91,9 +230,19 @@ class App extends React.Component {
 
           </ElementLoader>
 
+          <BarChart
+            title={ 'Bar Chart' }
+            data={ trajectoryData }
+            xProp={ 'year' }
+            yProp={ 'sale' }
+          />
 
 
-        </div>
+        <LineChart
+          data={ lineData }
+          initialWidth={ 500 }
+          xProp='date'
+          yProp='close'/>
 
       </div>
     )
