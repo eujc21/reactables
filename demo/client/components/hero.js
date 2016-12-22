f20ab0391cb8efeb25f19281525e81d06dc3bb9e
Phil <phil3903@gmail.com>
2016-12-22 11:00:41 -0500

    Added hero

---
+++ demo/client/components/hero.js
@@ -0,0 +1,29 @@
+import React from 'react'
+const Hero =({title, tagline})=>{
+
+  const style = {
+    base: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: 600,
+      color: 'white',
+      backgroundColor: 'blue'
+    },
+    title: {
+      fontSize: 36
+    }
+
+  }
+
+  return (
+    <div style={ style.base }>
+      <h1 style={ style.title }>{ title }</h1>
+      <p>{ tagline }</p>
+    </div>
+  )
+}
+
+export default Hero
