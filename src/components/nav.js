37db358217940bb64a70266ba4083bdcb623e9a6
Phil <phil3903@gmail.com>
2016-12-22 11:02:08 -0500

    Nav is now 'sticky'

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,72 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { getTop } from '../utils/utils'
 
-export const Nav =({ children })=>{
+export class Nav extends React.Component {
 
-  const style = {
-    base: {
-      position: 'relative',
-      width: 300,
-      padding: '0 10px',
-    },
-    nav: {
-      position: 'fixed',
-      width: '100%',
-      listStyleType: 'none',
-      zIndex: 10000
-    }
+  static propTypes = {
+    initialTop: PropTypes.number
   }
 
-  return (
-    <div style={ style.base }>
-      <ul style={ style.nav }>
-        { children }
-      </ul>
-    </div>
-  )
+  state = { top: undefined }
+
+  componentDidMount(){
+    // set top and offset
+    this.top = getTop(this.container)
+    this.paddingTop = parseInt(window.getComputedStyle(this.container.parentNode).paddingTop)
+
+    window.addEventListener('scroll', this.adjustOffset)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.adjustOffset)
+  }
+
+  adjustOffset =()=>{
+    const top = window.pageYOffset <= (this.top - this.paddingTop)
+      ? (this.top - window.pageYOffset)
+      : this.paddingTop
+
+    this.setState ({top})
+  }
+
+  render(){
+    const { children } = this.props
+
+    const style = {
+      base: {
+        position: 'relative',
+        width: 300,
+      },
+      nav: {
+        position: 'fixed',
+        top: this.state.top,
+        listStyleType: 'none',
+        border: '1px solid #f5f5f5',
+        width: 'inherit',
+        padding: 0,
+        margin: 0,
+        zIndex: 10000,
+        boxSizing: 'border-box'
+      }
+    }
+
+    return (
+      <div ref={ container => this.container = container } style={ style.base }>
+        <ul style={ style.nav }>
+          { children }
+        </ul>
+      </div>
+    )
+  }
+
 }
 
 export const NavLink =({to, children})=>{
+
+  const style = {}
+
   return(
-    <li>
+    <li style={ style }>
       <a href={ to }>{ children }</a>
     </li>
   )
