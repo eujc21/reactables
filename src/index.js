9e867eec9bc56f3231d6ec69b07b4fc283e1b542
Phil <phil3903@gmail.com>
2016-12-23 12:30:25 -0500

    updated exports

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 export { ProgressBar } from './components/progress_bar'
 export { Table } from './components/table'
-export { Dropdown } from './components/dropdown/dropdown'
-export { DropdownItem } from './components/dropdown/dropdown_item'
-export { Select } from './components/select/select'
-export { SelectOption } from './components/select/select_option'
+export { Dropdown, DropdownOption } from './components/dropdown'
+export { Select, SelectOption } from './components/select'
 export { Input } from './components/input'
 export { CountLabel } from './components/count_label'
 export { DateTimePicker } from './components/datepicker/date_time_picker'
 export { Button } from './components/button'
 export { KonamiCode } from './components/konami_code'
 export { HeatBar } from './components/heat_bar'
-export { Navbar } from './components/navbar'
+export { Navbar, NavbarLink } from './components/navbar'
 export { Nav, NavLink } from './components/nav'
 export HiddenPanel from './components/hidden_panel'
 export Code from './components/code'
 
-export { BarChart } from './components/charts/bar'
-export { LineChart} from './components/charts/line'
-export { TrajectoryChart } from './components/charts/trajectory'
-export { Sankey } from './components/charts/sankey'
-
-
 export ElementLoader from './components/element_loader/element_loader'
 export elementLoaderMiddleware from './components/element_loader/element_loader_middleware'
 export elementLoaderReducer from './components/element_loader/element_loader_reducer'
+
+export { BarChart } from './charts/bar'
+export { LineChart} from './charts/line'
+export { TrajectoryChart } from './charts/trajectory'
+export { Sankey } from './charts/sankey'
+
+
+
 
