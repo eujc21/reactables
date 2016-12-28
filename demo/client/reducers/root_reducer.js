b972795cf9fe28f4ad545bc74b6685ca5e365de0
Phil <phil3903@gmail.com>
2016-12-28 12:41:14 -0500

    Split app state

--- demo/client/reducers/root_reducer.js
+++ demo/client/reducers/root_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { UPDATE_INPUT_TEXT, INCREMENT_COMPLETED, TOGGLE_PANEL } from '../actions/demo_actions'
 import { elementLoaderReducer } from '../../../src/index'
+import moment from 'moment'
 
 
 function errorMessage(state = null, action) {
@@ -16,14 +17,16 @@ function errorMessage(state = null, action) {
   return state
 }
 
-const initialAppState = {
+
+
+const initialComponentsState = {
   tableData: [{a:1, b:2, c: 3}, {a:2, b: 3, c: 4}, {a:3, b: 4, c: 5}],
   completed: 0,
   outOf: 100,
   isPanelVisible: false
 }
 
-function app(state = initialAppState, action) {
+function components(state = initialComponentsState, action) {
 
   switch(action.type){
     case UPDATE_INPUT_TEXT:
@@ -42,8 +45,118 @@ function app(state = initialAppState, action) {
   }
 }
 
+
+
+const now = moment()
+const initialChartsState = {
+  lineData: [
+    {
+      name: 'Set 1',
+      values: [
+        {
+          count: 12,
+          date: now
+        },
+        {
+          count: 20,
+          date: now.clone().add(1, 'day')
+        },
+        {
+          count: 55,
+          date: now.clone().add(2, 'days')
+        },
+        {
+          count: 25,
+          date: now.clone().add(3, 'days')
+        },
+        {
+          count: 35,
+          date: now.clone().add(4, 'days')
+        }
+      ]
+    }
+  ] ,
+  barData: [
+    {
+      count: 202,
+      year: 2000
+    },
+
+    {
+      count: 179,
+      year: 2002
+    },
+
+    {
+      count: 154,
+      year: 2003
+    },
+
+    {
+      count: 215,
+      year: 2001
+    },
+
+    {
+      count: 260,
+      year: 2010
+    }
+  ],
+  sankeyData: {
+    nodes: [
+      { name: 'Node 0'},
+      { name: 'Node 1'},
+      { name: 'Node 2'},
+      { name: 'Node 3'},
+      { name: 'Node 4'},
+    ],
+    links: [
+      {
+        source: 0,
+        target: 4,
+        value: 10
+      },
+      {
+        source: 1,
+        target: 4,
+        value: 10
+      },
+      {
+        source: 2,
+        target: 4,
+        value: 10
+      },
+      {
+        source: 3,
+        target: 4,
+        value: 10
+      },
+    ]
+  }
+}
+
+function charts(state = initialChartsState, action){
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
+
+
+const initialLayoutState = {}
+
+function layout(state = initialLayoutState, action){
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
 const rootReducer = combineReducers({
-  app,
+  components,
+  charts,
+  layout,
   errorMessage,
   elementLoaderReducer,
 });
