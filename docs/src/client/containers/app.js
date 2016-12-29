6e223f6f3d191d76bf84667fbad8f3f945bca72f
Phil <phil3903@gmail.com>
2016-12-29 10:57:45 -0500

    Added scrollTop detection

--- docs/src/client/containers/app.js
+++ docs/src/client/containers/app.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { Navbar, NavbarLink } from '../../../../src/index'
+import { setPageScrollPosition } from '../actions/demo_actions'
 import Hero from '../components/hero'
 
 class App extends React.Component {
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handlePagePosition)
+  }
+
+  handlePagePosition =()=>{
+    this.props.setPageScrollPosition(document.body.scrollTop)
+  }
 
   handleRoute =(path)=>{
     browserHistory.push(path)
@@ -12,12 +21,16 @@ class App extends React.Component {
 
   render() {
 
+    const NAV_BREAK_POINT = 420
+    const { pageScrollPosition } = this.props
+
     const style = {
       base: {},
       navbar: {
         base: {
-          backgroundColor: 'blue',
+          backgroundColor: pageScrollPosition < NAV_BREAK_POINT ? 'transparent' : '#32b1d1',
           color: 'white',
+          transition: 'background-color 10ms ease-in-out'
         },
         title: {
           fontWeight: 600
@@ -32,7 +45,7 @@ class App extends React.Component {
           <NavbarLink to={ ()=> this.handleRoute('/') } styles={{ link: style.navbar.title }}>Reactables</NavbarLink>
           <NavbarLink to={ ()=> this.handleRoute('/components') }>Components</NavbarLink>
           <NavbarLink to={ ()=> this.handleRoute('/charts') }>Charts</NavbarLink>
-          <NavbarLink to={ ()=> this.handleRoute('/layout') }>Layout</NavbarLink>
+          {/*<NavbarLink to={ ()=> this.handleRoute('/layout') }>Layout</NavbarLink>*/}
         </Navbar>
 
         <Hero
@@ -49,11 +62,11 @@ class App extends React.Component {
 
 function mapStateToProps(state){
   return {
+    pageScrollPosition: state.app.pageScrollPosition
   }
 }
 
 export default connect(mapStateToProps, {
-
-
+  setPageScrollPosition
 })(App)
 
