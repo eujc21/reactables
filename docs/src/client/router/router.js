c1f463245a38262560c6d890b908c22ac7d89464
Phil <phil3903@gmail.com>
2017-01-07 17:21:03 -0500

    Build

--- docs/src/client/router/router.js
+++ docs/src/client/router/router.js
@@ -1,60 +1,62 @@
 import React from 'react'
-import Router from 'react-router/lib/Router'
+import { Router, Route, IndexRoute } from 'react-router'
 import App from '../containers/app'
+import Components from '../containers/components'
+import Charts from '../containers/charts'
 
 export default function configureRouter(history, store){
-
-  function errorLoading(err){
-    console.error('Dynamic page loading failed', err)
-  }
-
-  function loadRoute(cb) {
-    return (module) => cb(null, module.default)
-  }
 
   function setScrollTop(){
     document.body.scrollTop = 0
   }
 
-  // {
-  //   path: '/components',
-  //     getComponent(location, cb) {
-  //     System
-  //       .import('../containers/components')
-  //       .then(loadRoute(cb))
-  //       .then(setScrollTop)
-  //       .catch(errorLoading)
-  //   }
-  // },
-
   return (
-    <Router history={ history } routes={[
-      {
-        component: App,
-        path: '/',
-        onEnter: ()=>{ },
-        indexRoute: {
-          getComponent(location, cb) {
-            System
-              .import('../containers/components')
-              .then(loadRoute(cb))
-              .then(setScrollTop)
-              .catch(errorLoading)
-          }
-        },
-        childRoutes: [
-          {
-            path: '/charts',
-            getComponent(location, cb) {
-              System
-                .import('../containers/charts')
-                .then(loadRoute(cb))
-                .then(setScrollTop)
-                .catch(errorLoading)
-            }
-          }
-        ]
-      }
-    ]} />
+    <Router history={ history }>
+      <Route path={'/'} component={ App }>
+        <IndexRoute component={ Components } onEnter={ setScrollTop }/>
+        <Route path={ '/charts' } component={ Charts } onEnter={ setScrollTop }/>
+      </Route>
+    </Router>
   )
+
+  // function errorLoading(err){
+  //   console.error('Dynamic page loading failed', err)
+  // }
+  //
+  // function loadRoute(cb) {
+  //   return (module) => cb(null, module.default)
+  // }
+  //
+  // return (
+  //   <Router history={ history } routes={[
+  //     {
+  //       component: App,
+  //       path: '/',
+  //       onEnter: ()=>{ },
+  //       indexRoute: { onEnter: (nextState, replace) => replace('/components') },
+  //       childRoutes: [
+  //         {
+  //           path: '/components',
+  //           getComponent(location, cb) {
+  //             System
+  //               .import('../containers/components')
+  //               .then(loadRoute(cb))
+  //               .then(setScrollTop)
+  //               .catch(errorLoading)
+  //           }
+  //         },
+  //         {
+  //           path: '/charts',
+  //           getComponent(location, cb) {
+  //             System
+  //               .import('../containers/charts')
+  //               .then(loadRoute(cb))
+  //               .then(setScrollTop)
+  //               .catch(errorLoading)
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   ]} />
+  // )
 }
