65bbb52d2111977a4eaffecf0b68e067c13b6728
Phil <phil3903@gmail.com>
2017-01-24 14:10:00 -0500

    Added responsive behavior

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { Button } from './button'
 import merge from 'lodash/merge'
 
 class Navbar extends React.Component {
@@ -17,14 +18,16 @@ class Navbar extends React.Component {
 
   state = {
     isMobile: false,
-    isMobileMenuVisible: false
+    isMenuVisible: false
   }
 
   componentDidMount() {
     const { responsiveWidth } =  this.props
+
     const minWidth = responsiveWidth === 'auto'
       ? this.calculateLinkWidth()
       : responsiveWidth
+
     this.mediaQuery = window.matchMedia(`(min-width: ${ minWidth }px)`)
     this.mediaQuery.addListener(this.updateIsMobile)
     this.updateIsMobile()
@@ -35,9 +38,8 @@ class Navbar extends React.Component {
   }
 
   calculateLinkWidth=()=>{
-    const { leftLinks, rightLinks } = this
-    const leftWidth = parseInt(window.getComputedStyle(leftLinks).width) || 0
-    const rightWidth = parseInt(window.getComputedStyle(rightLinks).width) || 0
+    const leftWidth = parseInt(window.getComputedStyle(this.leftLinks).width) || 0
+    const rightWidth = parseInt(window.getComputedStyle(this.rightLinks).width) || 0
     return leftWidth + rightWidth
   }
 
@@ -45,38 +47,44 @@ class Navbar extends React.Component {
     const isMobile = !this.mediaQuery.matches
     this.setState({
       isMobile,
-      isMobileMenuVisible: false
+      isMenuVisible: false
     })
   }
 
   toggleMobileMenu =()=>{
     this.setState({
-      isMobileMenuVisible: !this.state.isMobileMenuVisible
+      isMenuVisible: !this.state.isMenuVisible
+    })
+  }
+
+  handleMenuClick =()=>{
+    this.setState({
+      isMenuVisible: false
     })
   }
 
   render(){
 
     const { styles, children } = this.props
-    const { isMobile, isMobileMenuVisible } = this.state
+    const { isMobile, isMenuVisible } = this.state
 
     let style = {
-      base:{
+      base: {
         position: 'fixed',
+        width: '100%',
+        zIndex: 9999,
+      },
+      bar:{
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
-        zIndex: 9999,
         width: '100%',
         height: 70,
         backgroundColor: '#000000',
         top: 0,
         left: 0,
       },
-      mobileBase:{
-
-      },
-      links: {
+      linkContainer: {
         display: 'flex',
         alignItems: 'center',
         padding: 0,
@@ -84,98 +92,202 @@ class Navbar extends React.Component {
         height: '100%',
         listStyleType: 'none'
       },
-      mobileLinks: {
-
+      menuButton: {
+        margin: '0 15px',
+        padding: '5px 10px',
+      },
+      menu:{
+        backgroundColor: '#525252',
+        margin: 0,
+        paddingLeft: 0,
+        visibility: isMenuVisible ? 'visible' : 'hidden',
+        overflowY: 'hidden',
+        //transition: 'all 0.7s ease-in-out',
+        boxShadow: '0px 2px 4px 0px rgba(0,0,0, 0.35)'
       }
     }
 
     merge(style, styles)
 
-    const links = React.Children
-      .toArray(children)
-      .reduce((obj, link, i)=>{
-
-        if(link.props.align === 'left'){
-
-          obj.left.push(React.cloneElement(
-            link, { isMobile }
-          ))
-
-        } else {
-
-          obj.right.push(React.cloneElement(
-            link, { isMobile }
-          ))
-
-        }
-        return obj
-
-      }, { left: [], right: []})
-
     return(
       <div style={ style.base }>
-
-        <ul ref={ ul => this.leftLinks = ul } style={ style.links }>{ links.left }</ul>
-        <ul ref={ ul => this.rightLinks = ul } style={ style.links }>{ links.right }</ul>
-
+        { isMobile
+          ? this.renderMobile(children, style)
+          : this.renderDesktop(children, style)
+        }
       </div>
     )
   }
 
-  renderDesktopLinks(){
+  renderDesktop(children, style){
+
+    const links = React.Children
+      .toArray(children)
+      .reduce((obj, link)=>{
+        const { append } = link.props
+        obj[append].push(React.cloneElement(
+          link, { isMobile: false }
+        ))
+        return obj
+      }, { left: [], right: []})
+
+    return(
+      <div style={ style.bar }>
+        <ul ref={ ul => this.leftLinks = ul } style={ style.linkContainer }>{ links.left }</ul>
+        <ul ref={ ul => this.rightLinks = ul } style={ style.linkContainer }>{ links.right }</ul>
+      </div>
+      )
 
   }
 
-  renderMobileLinks(){
+  renderMobile(children, style){
 
+    const links = React.Children
+      .toArray(children)
+      .reduce((obj, link)=>{
+        const { appendResponsive } = link.props
+        obj[appendResponsive].push(React.cloneElement(
+          link, { isMobile: appendResponsive !== 'bar' }
+        ))
+        return obj
+      }, { bar: [], menu: [] })
+
+
+    return(
+      <div>
+
+        <div style={ style.bar }>
+          <ul style={ style.linkContainer }>
+            { links.bar }
+          </ul>
+          <Button
+            text={ <i className="icon-hamburger"/>}
+            styles={ style.menuButton }
+            onClick={ this.toggleMobileMenu }
+          />
+        </div>
+
+        <ul
+          ref={ ul => this.menu = ul }
+          style={ style.menu }
+          onClick={ this.handleMenuClick }
+        >
+          { links.menu }
+        </ul>
+
+      </div>
+    )
   }
 }
+
+
+
+
+
 
 class NavbarLink extends React.Component {
 
   static propTypes =  {
     append: PropTypes.oneOf(['left', 'right']),
     appendResponsive: PropTypes.oneOf(['bar', 'menu', 'hide']),
-    to: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired
+    to: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
+    isActive: PropTypes.bool,
   }
 
   static defaultProps = {
-    align: 'left'
+    append: 'left',
+    appendResponsive: 'menu',
+    isActive: false
+  }
+
+  state = { isHovered: false }
+
+  handleHover =()=>{
+    this.setState({
+      isHovered: !this.state.isHovered
+    })
+  }
+
+  setLinkColor =(style)=> {
+
+    const { isActive, isMobile } = this.props
+    const { isHovered } = this.state
+
+    if(isMobile) {
+      if(isActive)
+        merge(style.menuLink.base, style.menuLink.active)
+      if(isHovered)
+        merge(style.menuLink.base, style.menuLink.hover)
+    } else {
+      if(isActive)
+        merge(style.link.base, style.link.active)
+      if(isHovered)
+        merge(style.link.base, style.link.hover)
+    }
+
+    return style
   }
 
   render() {
 
-    const { children, to, append, appendResponsive, isMobile, styles } = this.props
+    const { children, to, isMobile, styles } = this.props
 
-    const style = {
+    let style = {
       base: {
         height: '100%',
         display: 'inline',
         padding: 0,
-
       },
-      mobileBase: {},
       link: {
-        display: 'flex',
-        alignItems: 'center',
-        height: '100%',
-        margin: 0,
-        padding: '0 15px',
-        fontSize: 14,
-        textDecoration: 'none',
-        color: 'white',
-        cursor: 'pointer'
+        base:{
+          display: 'flex',
+          alignItems: 'center',
+          height: '100%',
+          margin: 0,
+          padding: '0 15px',
+          fontSize: 14,
+          textDecoration: 'none',
+          color: 'white',
+          cursor: 'pointer',
+        },
+        active: {
+          backgroundColor: '#cecece'
+        },
+        hover: {
+          backgroundColor: '#cecece'
+        }
       },
-      mobileLink: {}
+      menuLink: {
+        base:{
+          display: 'flex',
+          alignItems: 'center',
+          borderBottom: '1px solid #282828',
+          height: '100%',
+          margin: 0,
+          padding: '20px 15px',
+          fontSize: 14,
+          textDecoration: 'none',
+          color: 'white',
+          cursor: 'pointer'
+        },
+        active: {
+          backgroundColor: '#cecece'
+        },
+        hover: {
+          backgroundColor: '#cecece'
+        }
+
+      }
     }
 
     merge(style, styles)
+    this.setLinkColor(style)
 
     return (
-      <li style={ style.base }>
+      <li style={ style.base } onMouseEnter={ this.handleHover } onMouseLeave={ this.handleHover }>
         { typeof to === 'string'
-          ? <a style={ style.link } href={ to }>{ children }</a>
-          : <a style={ style.link } onClick={ to }>{ children }</a>
+          ? <a style={ isMobile ? style.menuLink.base : style.link.base } href={ to }>{ children }</a>
+          : <a style={ isMobile ? style.menuLink.base : style.link.base } onClick={ to }>{ children }</a>
         }
       </li>
     )
