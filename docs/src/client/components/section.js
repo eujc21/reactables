810fc6fe37c42d1c0413f8d45991439096226b8b
Phil <phil3903@gmail.com>
2017-01-26 12:16:37 -0500

    flex-wrap

--- docs/src/client/components/section.js
+++ docs/src/client/components/section.js
@@ -5,11 +5,13 @@ const Section =({ id, name, children })=>{
   const style = {
     base:{
       //flex: 1,
-      width: '100%',
+      //width: '100%',
+      display: 'flex',
+      flexWrap: 'wrap',
       maxWidth: 500,
       margin: '60px 0'
     },
-    heading: { marginTop: 0, paddingTop: 0 },
+    heading: { width: '100%',marginTop: 0, paddingTop: 0 },
     span: {
       //Adjusted for DOM id link
       marginTop: '-130px',
